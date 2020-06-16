38a4c7431ef9c27ace58d44cff2db5d8e5757a20
echoplus <qazwsxedc219@qq.com>
2020-06-16 02:45:30 -0700

    OperationExcle

--- common/operation_excel.py
+++ common/operation_excel.py
@@ -4,3 +4,16 @@ data = xlrd.open_workbook('../data/API.xlsx')
 tables = data.sheets()[0]
 print(tables.nrows)
 print(tables.cell_value(2, 3))
+
+class OperationExcel:
+    def __init__(self,file_name,sheet_id):
+        self.data = self.get_data(file_name,sheet_id)
+
+    def get_data(self,file_name,sheet_id):
+        data = xlrd.open_workbook(file_name)
+        tables = data.sheets()[sheet_id]        # 先拿sheet1、sheet2、sheet3等表
+        return tables
+
+if __name__ == '__main__':
+    opers = OperationExcel()
+    opers.get_data()
